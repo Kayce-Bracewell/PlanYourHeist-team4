1d4f06fa92e69bb375bbbb826d0af3da8ac2cee6
Adameia Ramsey <adameiaproductions@gmail.com>
2024-04-10 09:54:52 -0500

    phase 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Plan Your Heist!");
+
+
+Console.WriteLine("Please enter team member's name:");
+string recruitName = Console.ReadLine();
+Console.WriteLine("Please enter team member's skill level:");
+int recruitSkillLevel = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter team member's courage factor:");
+decimal recruitCourageFactor = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine(@$"Recruited Team Member information:
+Name: {recruitName}
+Skill level: {recruitSkillLevel}
+Courage factor: {recruitCourageFactor}");
