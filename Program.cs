033d9f39feb518b2ab403f3a68237eee9ac23f70
Adameia Ramsey <adameiaproductions@gmail.com>
2024-04-10 10:11:19 -0500

    phase 2

--- Program.cs
+++ Program.cs
@@ -1,14 +1,40 @@
-﻿Console.WriteLine("Plan Your Heist!");
+﻿List<TeamMember> teamMembers = new List<TeamMember>();
+
+Console.WriteLine("Plan Your Heist!");
 
 
-Console.WriteLine("Please enter team member's name:");
-string recruitName = Console.ReadLine();
-Console.WriteLine("Please enter team member's skill level:");
-int recruitSkillLevel = int.Parse(Console.ReadLine());
-Console.WriteLine("Please enter team member's courage factor:");
-decimal recruitCourageFactor = decimal.Parse(Console.ReadLine());
+bool ContinueBool = true;
+while (ContinueBool == true)
+{
+    Console.WriteLine("Please enter team member's name:");
+    string recruitName = Console.ReadLine();
+    if (recruitName == "")
+    {
+        ContinueBool = false;
+        break;
+    }
+    Console.WriteLine("Please enter team member's skill level:");
+    int recruitSkillLevel = int.Parse(Console.ReadLine());
+    Console.WriteLine("Please enter team member's courage factor:");
+    decimal recruitCourageFactor = decimal.Parse(Console.ReadLine());
 
-Console.WriteLine(@$"Recruited Team Member information:
-Name: {recruitName}
-Skill level: {recruitSkillLevel}
-Courage factor: {recruitCourageFactor}");
+    Console.WriteLine(@$"Recruited Team Member information:
+    Name: {recruitName}
+    Skill level: {recruitSkillLevel}
+    Courage factor: {recruitCourageFactor}");
+
+    teamMembers.Add(
+        new TeamMember
+        {
+            Name = recruitName,
+            SkillLevel = recruitSkillLevel,
+            CourageFactor = recruitCourageFactor
+        }
+    );
+}
+
+Console.WriteLine($"Number of team members: {teamMembers.Count}");
+foreach (TeamMember teamMember in teamMembers)
+{
+    Console.WriteLine($"{teamMember.Name} has a skill level of {teamMember.SkillLevel} and a courage factor of {teamMember.CourageFactor}");
+}
